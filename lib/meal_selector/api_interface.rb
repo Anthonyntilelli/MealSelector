14e3be84b90decb2bb4d898cf4f38a66946ca7ef
Anthony Tilelli <Anthony@TIlelli.me>
2019-11-18 21:00:29 -0500

    add base api layer and meal object
    squash into one merge for api layer and meal object

--- lib/meal_selector/api_interface.rb
+++ lib/meal_selector/api_interface.rb
@@ -1,59 +1,146 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'json'
+require_relative "meal.rb"
+
 module MealSelector
-  class API_INTERFACE
-    API_ENDPOINT = "https://www.themealdb.com/api"
+  # Communicates with Mealdb database for meal data
+  # Network issue will raised exceptions which are expected to handle by caller
+  class ApiInterface
+    API_ENDPOINT = 'https://www.themealdb.com/api/json'
 
     def initialize(key, version)
       # Sets API key and Version
-      
       # Set api_key and check its correct format
-      raise "Key must a string" unless key.is_a?(String)
-      Integer(key) rescue raise "API must be a number"
-      warn("Warning: API key `1` is only for development") if key == "1"
-      @API_KEY = KEY
+      raise 'Key must a string' unless key.is_a?(String)
 
-      # sets api_version
-      raise "version must be integer above or equal to one" unless version.is_a?(Integer) && version.to_i >= 1
-      @API_VERSION = version
+      begin
+        Integer(key)
+      rescue ArgumentError
+        raise 'API must be a number'
+      end
+      warn('Warning: API key `1` is only for development') if key == '1'
+      raise 'version must be integer above or equal to one' \
+      unless version.is_a?(Integer) && version.to_i >= 1
+      @version = version
+      @key = key
     end
 
     def search_meal_name(name)
       # Search meal by name
-      # https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+      # EXAMPLE: https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+      raise 'Name is not a string' unless name.is_a?(String)
+      name = name.gsub(" ","%20")
+
+      raw_content = nil
+      connection = open("#{api_url}search.php?s=#{name}").each do |json|
+        raw_content = json
+      end
+      connection.close
+      json_meal = JSON.parse(raw_content)
+      MealSelector::Meal.create_from_array(json_meal)
     end
 
     def meal_by_id(id)
       # Lookup full meal details by id
-      # https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+      # EXAMPLE: https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+
+      raise "id is not an Integer (#{id.class})" unless id.is_a?(Integer)
+      existing_meal = MealSelector::Meal.find_by_id(id.to_s)
+      return [ existing_meal ] if existing_meal
+
+      # Not already looked up
+      raw_content = nil
+      connection = open("#{api_url}lookup.php?i=#{id}").each do |json|
+        raw_content = json
+      end
+      connection.close
+      json_meal = JSON.parse(raw_content)
+      MealSelector::Meal.create_from_array(json_meal)
     end
 
     def random_meal
       # Lookup a single random meal
-      # https://www.themealdb.com/api/json/v1/1/random.php
+      # EXAMPLE: https://www.themealdb.com/api/json/v1/1/random.php
+      raw_content = nil
+      connection = open("#{api_url}random.php").each do |json|
+        raw_content = json
+      end
+      connection.close
+      json_meal = JSON.parse(raw_content)
+      MealSelector::Meal.create_from_array(json_meal)
     end
 
-    def list_all_meal_categories
-        # List all meal categories
-        # https://www.themealdb.com/api/json/v1/1/categories.php
+    def populate_categories
+      # Gets List of Categories for meals and set them.
+      # List all Categories
+      # EXAMPLE: https://www.themealdb.com/api/json/v1/1/list.php?c=list
+
+      raw_categories = nil
+      connection = open("#{api_url}list.php?c=list").each do |json|
+        raw_categories = json
+      end
+      connection.close
+      MealSelector::Meal.set_categories(JSON.parse(raw_categories)['meals'])
     end
 
-    def populate_lists
-        # List all Categories and Ingredients
-        # https://www.themealdb.com/api/json/v1/1/list.php?c=list
-        # https://www.themealdb.com/api/json/v1/1/list.php?i=list
+    def search_by_ingredient(primary_ingredient)
+      # Search by primary main ingredient
+      # EXAMPLE: https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
+      raise 'primary_ingredient is not a string' unless primary_ingredient.is_a?(String)
+      primary_ingredient = primary_ingredient.gsub(" ","%20")
+
+      raw_content = nil
+      connection = open("#{api_url}filter.php?i=#{primary_ingredient}").each do |json|
+        raw_content = json
+      end
+      connection.close
+      json_meal = JSON.parse(raw_content)
+      # partial reasults are returned by this api call.
+      json_meal['meals'].collect do | hash_meal |
+        meal_by_id(hash_meal['idMeal'].to_i)
+      end
     end
 
-    def meals_by_main_ingredient(main_ingredient)
-        # Filter by main ingredient
-        # https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
-    end
-    
     def meals_by_category(category)
-        # Filter by Category
-        # https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
+      # Filter by Category
+      # EXAMPLE: https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
+      populate_categories if MealSelector::Meal.categories.empty? || MealSelector::Meal.categories.nil?
+      raise "#{category} is not a valid category" unless MealSelector::Meal.categories.include?(category)
+
+      raw_content = nil
+      connection = open("#{api_url}filter.php?c=#{category}").each do |json|
+        raw_content = json
+      end
+      connection.close
+      json_meal = JSON.parse(raw_content)
+      # partial reasults are returned by this api call.
+      json_meal['meals'].collect do | hash_meal |
+        meal_by_id(hash_meal['idMeal'].to_i)
+      end
     end
-    
-    def save_api_key(path = "~/.Mealdbkey")
-      # Saves api_key to a file
+
+    def save(path = "#{Dir.home}/.Mealdbkey")
+      # Saves ApiInterface to a file (will overwrite existing file)
+      File.open(path, 'w') { |file| file.write("version: #{@version}\nkey: #{@key}") }
+    end
+
+    def self.load(path = "#{Dir.home}/.Mealdbkey")
+      # Creates ApiInterface from a file
+      raise "File #{path} does not exist!" unless File.exist?(path)
+      raw_data = File.read(path).chomp
+      raw_data = raw_data.split
+      raise 'Incorrect format for Meal Api Key file' unless raw_data.count == 4
+      raise "Error finding version info (#{raw_data[0]})" unless raw_data[0] == 'version:'
+      raise "Error finding key info (#{raw_data[2]})" unless raw_data[2] == 'key:'
+      ApiInterface.new(raw_data[3], raw_data[1].to_i)
+    end
+
+    private
+
+    def api_url
+      API_ENDPOINT + "/v#{@version}/#{@key}/"
     end
 
   end
--- lib/meal_selector/meal.rb
+++ lib/meal_selector/meal.rb
@@ -1,15 +1,114 @@
+# frozen_string_literal: true
+
 module MealSelector
-    class Meal
-        # Data container for meals
-        @@all = []
+  # Data container for meals (raised Exceptions must be handled by caller)
+  class Meal
+    @@all = []
+    @@categories = []
+    attr_reader :id, :name, :category, :instructions, \
+                :type, :youtube, :ingredient
 
-        def self.all
-            @@all
-        end
+    def initialize(meal_hash)
+      # Converts hash into 1 meal object
+      # If meal object already exist it will return exiting object
+      raise 'meal_hash must be a hash' unless meal_hash.is_a?(Hash)
 
-        def save
-            @@all << self
-        end
+      raise 'meal_hash is not a hash for a meal' \
+            unless meal_hash['idMeal'].is_a?(String)
+
+      @id = meal_hash.delete('idMeal')
+      existing_meal = @@all.find { |meal| meal.id == @id }
+      return existing_meal if !existing_meal.nil?
+      @name = meal_hash.delete('strMeal')
+      @category = meal_hash.delete('strCategory')
+      @instructions = meal_hash.delete('strInstructions')
+      @type = meal_hash.delete('strTags')
+      @youtube = meal_hash.delete('strYoutube')
+      setup_ingredients(meal_hash)
+
+      # Prevent incomplete Meal
+      raise "Error setting up instructions" if @instructions.empty? || @instructions.nil?
+      raise "Error setting up ingredients" if @ingredient.empty? || @ingredient.nil?
+      save_and_freeze
     end
-end
+
+    def self.find_by_id(id)
+      # Returns meal by id if exists
+      # Return nil if it does not exist
+      raise 'id must be a string' unless id.is_a?(String)
+
+      @@all.find { |meal| meal.id == id }
+    end
+
+    def self.clear_all
+      @@all.clear
+      @@categories.clear
+    end
+
+    def self.categories_clear
+      @@categories.clear
+    end
+
+    def self.meal_clear
+      @@all.clear
+    end
+
+    def self.create_from_array(meals_hash)
+      # Create new meals from array of meals and returns array of Meal objects
+      raise 'meals_hash must me a hash' unless meals_hash.is_a?(Hash)
+
+      raise 'meals_hash must be an array of meals' \
+            unless meals_hash['meals'].is_a?(Array)
+
+      meals_hash['meals'].collect { |meal| Meal.new(meal) }
+    end
+
+    def self.set_categories(categories_arr)
+      # sets a list of meal categories by Api_interface
+      raise 'categories_arr must be an Array' unless categories_arr.is_a?(Array)
+      raise 'categories must not be empty' if  categories_arr.empty?
+
+      main_count = categories_arr.count
+      processed_categories = categories_arr.collect { |cat| cat["strCategory"] }.compact
+      # check for nil categories
+      raise "Count Error in categories" if main_count != processed_categories.count
+      @@categories = processed_categories
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.categories
+      @@categories
+    end
   
+    private
+
+    def save_and_freeze
+      # Saves meal to @@all if it does not already exist
+      if Meal.find_by_id(id).nil?
+        @@all << self
+        freeze
+        true
+      else
+        false
+      end
+    end
+
+    def setup_ingredients(left_over_meal_hash)
+      @ingredient = {} # 'strIngredient#' => 'strMeasure#'
+      left_over_meal_hash.each do |key, value|
+        next if value == '' || value == ' ' || value.nil? # prevent blank entries
+        next if key == '' || key == ' ' || key.nil? # prevent blank entries
+
+        case key
+        when /strIngredient.+/
+          location = key.gsub('strIngredient', '')
+          @ingredient[value] = left_over_meal_hash['strMeasure' + location.to_s]
+        end
+      end
+      @ingredient = @ingredient.compact
+    end
+  end
+end
